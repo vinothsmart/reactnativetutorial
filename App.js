ec64d513654cd94fa467565b99af98466da10280
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-17 11:30:50 +0530

    peoples name as list

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ListView } from 'react-native';
 
 export default function App() {
 
-  const [name, setName] = useState('Vino');
-  const [age, setAge] = useState(30);
+  // const [name, setName] = useState('Vino');
+  // const [age, setAge] = useState(30);
   // const [person, setPerson] = useState({ name: 'vinoth', age: 30 });
 
   // const clickHandler = () => {
@@ -12,30 +12,51 @@ export default function App() {
   //   setPerson({name:'Alamelu', age: 25})
   // }
 
-  return (
-    <View style={styles.container}>
-      <Text>Enter name:</Text>
-      <TextInput 
-        multiline
-        style={styles.input}
-        placeholder='e.g. Vinoth Kanna'
-        onChangeText={(val) => setName(val)}/>
-      
-      <TextInput 
-        keyboardType='numeric'
-        style={styles.input}
-        placeholder='e.g. 30'
-        onChangeText={(val) => setAge(val)}/>
+  const [people, setPeople] = useState([
+    { name: 'vinoth', key: '1'},
+    { name: 'kanna', key: '2'},
+    { name: 'ajitha', key: '3'},
+    { name: 'vignesh', key: '4'},
+    { name: 'anand', key: '5'},
+    { name: 'vivek', key: '6'},
+    { name: 'kali', key: '7'},
+  ]);
 
-      <Text>
-        name: {name}, age: {age}
-      </Text>
-      {/* <Text> My Name is {name}</Text>
-      <Text> His Name is {person.name}</Text>
-      <Text> His Age is {person.age}</Text>
-      <View style={styles.buttonContainer}>
-        <Button title='update state' onPress={clickHandler} />
-      </View> */}
+  return (    
+    // <View style={styles.container}>
+    //   <Text> My Name is {name}</Text>
+    //   <Text> His Name is {person.name}</Text>
+    //   <Text> His Age is {person.age}</Text>
+    //   <View style={styles.buttonContainer}>
+    //     <Button title='update state' onPress={clickHandler} />
+    //   </View>
+    // </View>
+    // <View style={styles.container}>
+    //   <Text>Enter name:</Text>
+    //   <TextInput 
+    //     multiline
+    //     style={styles.input}
+    //     placeholder='e.g. Vinoth Kanna'
+    //     onChangeText={(val) => setName(val)}/>
+      
+    //   <TextInput 
+    //     keyboardType='numeric'
+    //     style={styles.input}
+    //     placeholder='e.g. 30'
+    //     onChangeText={(val) => setAge(val)}/>
+
+    //   <Text>
+    //     name: {name}, age: {age}
+    //   </Text>
+    // </View>
+    <View style={styles.container}>
+      { people.map((item) => {
+        return(
+          <View>
+            <Text>{item.name}</Text>
+          </View>
+        )
+      })}
     </View>
   );
 }
@@ -44,17 +65,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
   // buttonContainer:{
   //   marginTop:20
   // }
-  input:{
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
-  }
+  // input:{
+  //   borderWidth: 1,
+  //   borderColor: '#777',
+  //   padding: 8,
+  //   margin: 10,
+  //   width: 200,
+  // }
 });
