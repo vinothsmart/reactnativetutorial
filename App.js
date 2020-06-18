ad755f8d9f57824b18bdeb17f0aca4fa1e4cc344
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-18 16:31:13 +0530

    onPress Cond added

--- App.js
+++ App.js
@@ -32,6 +32,9 @@ export default function App() {
 
   const pressHandler = (id) => {
     console.log(id);
+    setPeople((prevPeople)=> {
+      return prevPeople.filter(person => person.id != id)
+    })
   }
 
   return (
